da767a49595c562bff7069e61dd6748cee956c01
Cornelius Mund <cornelius.mund@gmail.com>
2015-09-14 16:30:59 +0200

    Improved send_gmail implementation.

--- tutorial/send_gmail.py
+++ tutorial/send_gmail.py
@@ -11,74 +11,78 @@ import os.path
 import webbrowser
 import base64
 import httplib2
-
-def GenerateOAuth2String(username, access_token, base64_encode=True):
-    """Generates an IMAP OAuth2 authentication string.
-    
-    See https://developers.google.com/google-apps/gmail/oauth2_overview
-    
-    Args:
-      username: the username (email address) of the account to authenticate
-      access_token: An OAuth2 access token.
-      base64_encode: Whether to base64-encode the output.
-    
-    Returns:
-      The SASL argument for the OAuth2 mechanism.
-    """
-    auth_string = 'user=%s\1auth=Bearer %s\1\1' % (username, access_token)
-    if base64_encode:
-        auth_string = base64.b64encode(auth_string)
-    return auth_string
-
-
-fname = "cred.json"
-
-credentials = ""
-store = Storage(fname)
-if os.path.isfile(fname):
-    store.acquire_lock()
-    credentials = store.locked_get()
-    store.release_lock()
-else:
-    flow = flow_from_clientsecrets('client_secret.json',
-                                   scope='https://mail.google.com/',
-                                   redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-
-    auth_uri = flow.step1_get_authorize_url()
-    webbrowser.open(auth_uri)
-    auth_code = raw_input('Enter the auth code: ')
-    credentials = flow.step2_exchange(auth_code)
-    store.acquire_lock()
-    store.locked_put(credentials)
-    store.release_lock()
-    credentials.set_store(store)
-
-user = 'cornelius.mund@gmail.com'
-if credentials.access_token_expired:
-    http = httplib2.Http()
-    credentials.refresh(http)
-auth_string = GenerateOAuth2String(user, credentials.access_token)
-
-
-# Import the email modules we'll need
 from email.mime.text import MIMEText
 
-msg = MIMEText('Test')
-
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'Test'
-msg['From'] = 'cornelius.mund@gmail.com'
-msg['To'] = 'cornelius.mund@d-fine.de'
-
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
-server = smtplib.SMTP('smtp.gmail.com:587')
-#server.set_debuglevel(True)
-server.ehlo()
-server.starttls()
-server.ehlo()
-server.docmd('AUTH', 'XOAUTH2 ' + auth_string)
-server.sendmail('cornelius.mund@gmail.com', ['cornelius.mund@d-fine.de'], msg.as_string())
-server.quit()
+class GmailSender(object):
+    
+    credentials = None
+    
+    def GenerateOAuth2String(self, username, access_token, base64_encode=True):
+        """Generates an IMAP OAuth2 authentication string.
+        
+        See https://developers.google.com/google-apps/gmail/oauth2_overview
+        
+        Args:
+          username: the username (email address) of the account to authenticate
+          access_token: An OAuth2 access token.
+          base64_encode: Whether to base64-encode the output.
+        
+        Returns:
+          The SASL argument for the OAuth2 mechanism.
+        """
+        auth_string = 'user=%s\1auth=Bearer %s\1\1' % (username, access_token)
+        if base64_encode:
+            auth_string = base64.b64encode(auth_string)
+        return auth_string
+    
+    
+    def load_oauth2_credentials(self, fname_secret, fname_creds):
+        store = Storage(fname_creds)
+        if os.path.isfile(fname_creds):
+            store.acquire_lock()
+            self.credentials = store.locked_get()
+            store.release_lock()
+        else:
+            flow = flow_from_clientsecrets(fname_secret,
+                                           scope='https://mail.google.com/',
+                                           redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+        
+            auth_uri = flow.step1_get_authorize_url()
+            webbrowser.open(auth_uri)
+            auth_code = raw_input('Enter the auth code: ')
+            self.credentials = flow.step2_exchange(auth_code)
+            store.acquire_lock()
+            store.locked_put(self.credentials)
+            store.release_lock()
+            self.credentials.set_store(store)
+    
+    
+    def send_mail(self, text):
+        if self.credentials == None:
+            self.load_oauth2_credentials('client_secret.json', "cred.json")
+    
+        user = 'cornelius.mund@gmail.com'
+        if self.credentials.access_token_expired:
+            http = httplib2.Http()
+            self.credentials.refresh(http)
+        auth_string = self.GenerateOAuth2String(user, self.credentials.access_token)
+        
+        msg = MIMEText(text)
+        
+        
+        # me == the sender's email address
+        # you == the recipient's email address
+        msg['Subject'] = 'Test'
+        msg['From'] = 'cornelius.mund@gmail.com'
+        msg['To'] = 'cornelius.mund@d-fine.de'
+        
+        # Send the message via our own SMTP server, but don't include the
+        # envelope header.
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        #server.set_debuglevel(True)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.docmd('AUTH', 'XOAUTH2 ' + auth_string)
+        server.sendmail('cornelius.mund@gmail.com', ['cornelius.mund@d-fine.de'], msg.as_string())
+        server.quit()
