bd32ffd7dfc83858672690e7f2d21a7f35d9fbd6
cornim <cornim@github.com>
2015-09-17 17:54:46 +0200

    Tried to optimize the pricing function.

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -21,39 +21,39 @@ class CarPipeline(object):
             if item['ez'] and item['km']:
                 base_price = 0
                 if spider.name == 'bmw':
-                    base_price = 70000
+                    base_price = 60000
                 if spider.name == "volvo":
                     base_price = 55000
                 acc_age = (2016-int(item['ez']) + int(item['km'])/25000.0)
                 base_price = base_price * math.exp(-0.12*acc_age)
                 base_price = base_price - int(item['dist'])*2
                 
-                base_price = self.mod_price(base_price, item['keyless'], 1000)                
-                base_price = self.mod_price(base_price, item['m_paket'], 2000)
-                base_price = self.mod_price(base_price, item['komf_sitz'], 600)
-                base_price = self.mod_price(base_price, item['act_sitz'], 500)
+                base_price = self.mod_price(base_price, item['keyless'], 2000)                
+                base_price = self.mod_price(base_price, item['m_paket'], 1500)
+                base_price = self.mod_price(base_price, item['komf_sitz'], 1000)
+                base_price = self.mod_price(base_price, item['act_sitz'], 800)
                 base_price = self.mod_price(base_price, item['sc_auto'], 250)
                 base_price = self.mod_price(base_price, item['gt_oeff'], 150)
-                base_price = self.mod_price(base_price, item['ah_kupp'], 500)
+                base_price = self.mod_price(base_price, item['ah_kupp'], 800)
                 
-                base_price = self.mod_price(base_price, item['adap_drive'], 1500)
-                base_price = self.mod_price(base_price, item['ddc'], 1000)
+                base_price = self.mod_price(base_price, item['adap_drive'], 2500)
+                base_price = self.mod_price(base_price, item['ddc'], 2000)
                 base_price = self.mod_price(base_price, item['stau_assi'], 5000)
                 base_price = self.mod_price(base_price, item['auto_h_k'], 150)
                 base_price = self.mod_price(base_price, item['fl_assi'], 700)
-                base_price = self.mod_price(base_price, item['adapt_kl'], 300)
+                base_price = self.mod_price(base_price, item['adapt_kl'], 500)
                 base_price = self.mod_price(base_price, item['act_lenk'], 0)
-                base_price = self.mod_price(base_price, item['act_ilenk'], 700)
+                base_price = self.mod_price(base_price, item['act_ilenk'], 1200)
                 
-                base_price = self.mod_price(base_price, item['HUD'], 1000)
-                base_price = self.mod_price(base_price, item['RTTI'], 1500)
-                base_price = self.mod_price(base_price, item['navi_prof'], 1000)
-                base_price = self.mod_price(base_price, item['speed_l_i'], 300)
-                base_price = self.mod_price(base_price, item['sw_warn'], 250)
+                base_price = self.mod_price(base_price, item['HUD'], 2000)
+                base_price = self.mod_price(base_price, item['RTTI'], 2500)
+                base_price = self.mod_price(base_price, item['navi_prof'], 3000)
+                base_price = self.mod_price(base_price, item['speed_l_i'], 800)
+                base_price = self.mod_price(base_price, item['sw_warn'], 400)
                 
                 base_price = self.mod_price(base_price, item['r_cam'], 100)
-                base_price = self.mod_price(base_price, item['s_view'], 250)
-                base_price = self.mod_price(base_price, item['p_assi'], 500)
+                base_price = self.mod_price(base_price, item['s_view'], 600)
+                base_price = self.mod_price(base_price, item['p_assi'], 1200)
                 
                 item['price_calc'] = round(base_price)
                 item['price_diff'] = item['price_calc'] - item['price']
